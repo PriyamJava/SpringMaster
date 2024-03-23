89f7e45cedeb59b377431a22b1ae8859a36afea5
Priyam Agarwal <priyam.a@accionlabs.com>
2024-03-23 17:22:01 +0530

    File Changed

--- src/main/java/com/priyam/learnspring/MasterSpring/MyFile.java
+++ src/main/java/com/priyam/learnspring/MasterSpring/MyFile.java
@@ -2,6 +2,6 @@ package com.priyam.learnspring.MasterSpring;
 
 public class MyFile {
     public static void main(String[] args) {
-        System.out.println("U r fucking asshole!!");
+        System.out.println("U r fucking assh!!");
     }
 }
