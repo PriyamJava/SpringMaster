6f5c6b2a7051d0a4ef2527385422f79d784bbfd5
Priyam Agarwal <priyam.a@accionlabs.com>
2024-03-23 17:06:30 +0530

    My First Commit

---
+++ src/test/java/com/priyam/learnspring/MasterSpring/MasterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyam.learnspring.MasterSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
