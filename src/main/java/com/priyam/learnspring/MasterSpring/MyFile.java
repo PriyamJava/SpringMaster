c56b25d7e122247da40973172dc46fbff2f5c13e
shriharsh <sholapurshriharsh@gmail.com>
2024-03-23 17:32:53 +0530

    deleted file

--- src/main/java/com/priyam/learnspring/MasterSpring/MyFile.java
+++
@@ -1,7 +0,0 @@
-package com.priyam.learnspring.MasterSpring;
-
-public class MyFile {
-    public static void main(String[] args) {
-        System.out.println("U r fucking asshole priyam!!");
-    }
-}
